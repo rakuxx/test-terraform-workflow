4b7e329ee5c306feec825f49f1a3ee287d0e5906
rakuxx <kwdowik@icloud.com>
2024-06-23 13:32:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+variable "content_value" {
+  type    = string
+  default = "Hello, world!"
+}
 
+variable "file_name" {
+  type    = string
+  default = "hello_world.txt"
+}
+
+resource "local_file" "hello_world" {
+  content  = var.content_value
+  filename = "${path.module}/${var.file_name}"
+}
